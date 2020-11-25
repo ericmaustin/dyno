5b43386d9ef044233b55139aeaf7d74106b46db7
Eric Austin <eric.m.austin@gmail.com>
2020-11-24 20:31:11 -0600

    updating aws tokens

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -9,6 +9,8 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 region = ${AWS_REGION}
 EOL
 
+echo ~/.aws/credentials
+
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
