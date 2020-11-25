444462f02973d03985b272106a6bc9d522f6f30f
Eric Austin <eric.m.austin@gmail.com>
2020-11-24 19:50:29 -0600

    update scripts/aws.sh

---
+++ scripts/aws.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+region = ${AWS_REGION}
+EOL
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
